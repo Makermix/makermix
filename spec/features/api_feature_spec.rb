d90fcb5f294cef906a6c3fc74027161459a1488c
Richard Lynch <rjlynch86@googlemail.com>
2015-05-13 14:26:37 +0100

    sets up testing

---
+++ spec/features/api_feature_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'api' do
+  context 'making a get request to the api with the correct params' do
+    scenario 'returns you the request json object' do
+      
+    end
+  end
+end
