cc656efedc1bf897f3e58799ab3aee6f098edb6f
James Miller <james.fl.miller@live.co.uk>
2015-05-13 16:47:58 +0100

    Adds API call test and passes param to get method

--- app/controllers/cohort_controller.rb
+++ app/controllers/cohort_controller.rb
@@ -1,5 +1,5 @@
 class CohortController < ApplicationController
   def view
-    render json: "Hello world"
+    render json: "Hello world, you asked to view cohort with id #{params[:id]}"
   end
 end
