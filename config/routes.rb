11d776d7880cb9e4b95bd1a0b191a2e90f8f2cff
James Miller <james.fl.miller@live.co.uk>
2015-05-14 14:08:31 +0100

    Adds route to show all cohorts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   # root to: "home#index"
   get 'cohorts/:id' => 'cohort#view'
+  get 'cohorts' => 'cohort#list'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/features/api_feature_spec.rb
+++ spec/features/api_feature_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-feature 'api' do
-  context 'making a get request to the api with the correct params' do
-    scenario 'returns you the request json object' do
+feature 'api calls' do
+  context 'to the cohorts route' do
+    scenario 'returns a cohort of users if given a cohort id' do
       cohort = Cohort.create(name: "March 2015")
       cohort.users.create(name: "James")
       get "/cohorts/#{cohort.id}"
@@ -13,6 +13,14 @@ feature 'api' do
     scenario 'returns 404 page if requested cohort does not exist' do
       get "/cohorts/1"
       expect(last_response.status).to eq(404)
+    end
+
+    scenario 'returns a list of cohorts if not given a cohort id' do
+      cohort1 = Cohort.create(name: 'March 2015')
+      cohort2 = Cohort.create(name: 'April 2015')
+      get "/cohorts"
+      expect(JSON.parse(last_response.body)[0]['name']).to eq(cohort1.name)
+      expect(JSON.parse(last_response.body)[1]['name']).to eq(cohort2.name)
     end
   end
 end
