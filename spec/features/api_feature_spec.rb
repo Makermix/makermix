8f55e478c622da2d477fb1bbb4e14864faa740cd
James Miller <james.fl.miller@live.co.uk>
2015-05-14 11:15:54 +0100

    API returns 404 if you request an invalid cohort

--- spec/features/api_feature_spec.rb
+++ spec/features/api_feature_spec.rb
@@ -4,9 +4,15 @@ feature 'api' do
   context 'making a get request to the api with the correct params' do
     scenario 'returns you the request json object' do
       cohort = Cohort.create(name: "March 2015")
-      cohort.users.create()
+      cohort.users.create(name: "James")
       get "/cohorts/#{cohort.id}"
-      expect(last_response.body).to eq(cohort.to_json)
+      expect(JSON.parse(last_response.body)['id']).to eq(cohort.id)
+      expect(JSON.parse(last_response.body)['users'][0]['name']).to eq('James')
+    end
+
+    scenario 'returns 404 page if requested cohort does not exist' do
+      get "/cohorts/1"
+      expect(last_response.status).to eq(404)
     end
   end
 end
