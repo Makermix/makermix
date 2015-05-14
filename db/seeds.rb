0c90df805783142b25ced34c2676f37d3127cb0e
James Miller <james.fl.miller@live.co.uk>
2015-05-14 10:10:57 +0100

    Adds seed file with starter data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+new_cohort = Cohort.create(name: 'March 2015')
+User.create(name: 'James', cohort_id: new_cohort.id)
+User.create(name: 'Milena', cohort_id: new_cohort.id)
+User.create(name: 'Caron', cohort_id: new_cohort.id)
+User.create(name: 'Sanjay', cohort_id: new_cohort.id)
+User.create(name: 'George', cohort_id: new_cohort.id)
+User.create(name: 'Sara', cohort_id: new_cohort.id)
+User.create(name: 'Rich', cohort_id: new_cohort.id)
+User.create(name: 'Tomi', cohort_id: new_cohort.id)
+User.create(name: 'Jacob', cohort_id: new_cohort.id)
+User.create(name: 'Massud', cohort_id: new_cohort.id)
