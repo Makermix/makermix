6b2ff02c876628a52d5b8b344f1271f3d0d36ded
James Miller <james.fl.miller@live.co.uk>
2015-05-15 10:09:51 +0100

    Tidies up changes made and standardises to push to Master

--- spec/controllers/cohort_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CohortController, type: :controller do
-
-end
--- spec/controllers/pairings_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PairingsController, type: :controller do
-
-end
